9ddcac1c8c7402ec03f3ed37c2bf066ad939f0ff
Sagar Rokade <33886845+sagarrokade006@users.noreply.github.com>
2018-05-28 00:49:59 +0530

    Update kenzo.sh

--- kenzo.sh
+++ kenzo.sh
@@ -14,3 +14,6 @@ git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-15
 git clone https://github.com/LineageOS/android_hardware_qcom_display -b lineage-15.1-caf-8952 hardware/qcom/display-caf/msm8952
 
 git clone https://gitlab.com/PixelExperience/prebuilts_gcc_linux-x86_aarch64_aarch64-linaro-7.2.git prebuilts/gcc/linux-x86/aarch64/aarch64-linaro-7.2
+
+export KBUILD_BUILD_HOST=Persistant
+export KBUILD_BUILD_USER=SagarRokade
