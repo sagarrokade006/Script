f450abd8039b0b9ab859fea1ff97c14b406004f7
Sagar Rokade <33886845+sagarrokade006@users.noreply.github.com>
2018-07-22 18:46:20 +0530

    Update kenzo.sh

--- kenzo.sh
+++ kenzo.sh
@@ -1,19 +1,19 @@
-#DEVICE
 
-git clone https://github.com/AmolAmrit/device_xiaomi_kenzo -b oreo device/xiaomi/kenzo
-git clone https://github.com/AmolAmrit/device_xiaomi_msm8956-common -b oreo device/xiaomi/msm8956-common
-git clone https://github.com/AmolAmrit/Escrima_kernel_xiaomi_msm8956 -b oreo kernel/xiaomi/msm8956
-git clone https://github.com/AmolAmrit/proprietary_vendor_xiaomi -b oreo vendor/xiaomi
+
+
+
+
+
 
 #HALS
 rm -rf hardware/qcom/audio-caf/msm8952
 rm -rf hardware/qcom/display-caf/msm8952
 rm -rf hardware/qcom/media-caf/msm8952
-git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-15.1-caf-8952 hardware/qcom/audio-caf/msm8952 
-git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-15.1-caf-8952 hardware/qcom/media-caf/msm8952 
-git clone https://github.com/LineageOS/android_hardware_qcom_display -b lineage-15.1-caf-8952 hardware/qcom/display-caf/msm8952
+git clone https://github.com/GZOSP/hardware_qcom_audio -b 8.1-caf-8952 ardware/qcom/audio-caf/msm8952 
+git clone https://github.com/GZOSP/hardware_qcom_media -b 8.1-caf-8952 -b lineage-15.1-caf-8952 hardware/qcom/media-caf/msm8952 
+git clone https://github.com/GZOSP/hardware_qcom_display -b 8.1-caf-8952 hardware/qcom/display-caf/msm8952
 
 git clone https://gitlab.com/PixelExperience/prebuilts_gcc_linux-x86_aarch64_aarch64-linaro-7.2.git prebuilts/gcc/linux-x86/aarch64/aarch64-linaro-7.2
 
-export KBUILD_BUILD_HOST=Persistant
-export KBUILD_BUILD_USER=SagarRokade
+
+
